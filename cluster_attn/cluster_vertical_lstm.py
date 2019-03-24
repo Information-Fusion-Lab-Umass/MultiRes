cc92edea991c1cbbf6307a4c34547658204fb357
Stone Zhang <stoneishere@gmail.com>
2019-03-24 17:23:35 -0400

    cluster attn (unfinished)

--- cluster_attn/cluster_vertical_lstm.py
+++ cluster_attn/cluster_vertical_lstm.py
@@ -76,10 +76,10 @@ class CVL(nn.Module):
         :param data: list: one batch data: [time_seq0, 37, 4]
         :return: tag_score: [1, 2]
         """
-        features = self.vertical_attn(data)  # (1, time_seq, cluster_num)
+        features = self.vertical_attn(data)  # (1, time_seq, cluster_num)    # (B, 3, T)
         #         print "FEAT"
         #         print features
-        features = self.LL(features)            # (1, time_seq, input_dim)
+        # features = self.LL(features)            # (1, time_seq, input_dim)  # (B, 3, input_dim)
         #         print "Features"
         #         print features
         #         print features.shape
@@ -93,13 +93,13 @@ class CVL(nn.Module):
         self.hidden = self.init_hidden(batch_size)
 
         packed_output, self.hidden = self.lstm(packed, self.hidden)
-        lstm_out = pad_packed_sequence(packed_output, batch_first=True)[0]
+        lstm_out = pad_packed_sequence(packed_output, batch_first=True)[0] # Bx3xH
 
         if self.attn_category == 'dot':
-            pad_attn = self.attn((lstm_out, torch.cuda.LongTensor(lengths)))
+            pad_attn = self.attn((lstm_out, torch.cuda.LongTensor(lengths)))  # (B,3,H) -> B,H
             #             print pad_attn
             #             print "TAG SPACE"
-            tag_space = self.hidden2tag(pad_attn)
+            tag_space = self.hidden2tag(pad_attn)  # (B, 2)
         #             print tag_space
         else:
             tag_space = self.hidden2tag(lstm_out[:, -1, :])
@@ -108,7 +108,9 @@ class CVL(nn.Module):
 
     def vertical_attn(self, data):
         """
-        :param data: list: (time_seq, 37, 4)
+        :param data: list: (B, 37, time_seq, 4)
+                     time_seq here is a fixed num. We preprocess the data so that for every datapoint it has the same
+                     time_seq. We call it max_time_len or T for short
         :return: imputation_attn: pytorch tensor: (1, time_seq, cluster_num)
         How to do this: We first visit all time_seq points,
                         in every time seq we have a [37, 4] tensor.
@@ -128,50 +130,111 @@ class CVL(nn.Module):
                         # on this [time_seq, 4] tensor and get a [time_seq, 1] output.
         """
 
-        time_seq = len(data)
 
-        raw_tbm = []  # (time_seq, num_features, 1)
 
-        for t in range(time_seq):
-            one_time = np.array(data[t])  # (37, 4)
+        # #Start
+        # BxFxTx4
+        # #TBM
+        # BxFxTx1
+        #
+        # #Clustering
+        # BxF1Xt
+        # BxF2Xt
+        # BxF3Xt
+        #
+        # #Pass to DotAttention Layer
+        # Bxt
+        # Bxt
+        # Bxt
+        #
+        # #Stack to get
+        # Bx3xt
 
-            local_tbm = []  # (37, 1)
+        #Pass to bilstm
+        B = len(data)
+        F = len(data[0])
+        T = len(data[0][0])
 
-            for f in range(self.num_features):
-                curr_feat = one_time[f]
-                # if(curr_feat[2]==1):
-                # TBM parameters
-                beta_val = 0.75
-                tau_val = 2
-                h_val = 0.4
-                m_t = curr_feat[2]
-                x_l = curr_feat[0]
-                x_m = curr_feat[1]
-                curr_delta_t = curr_feat[3]
-                b_t_dash = np.exp(-beta_val * curr_delta_t * 1.0 / tau_val)
-                if b_t_dash > h_val:
-                    b_t = 1
-                else:
-                    b_t = 0
-                feat_val = (1 - m_t) * x_l + m_t * (b_t * x_l + (1 - b_t) * x_m)
-                local_tbm.append([feat_val])
-            raw_tbm.append(local_tbm)
+        # tbm
+        raw_tbm = []  # (B, F, T)
+        for b in range(B):
+            one_batch = data[b]
+            local_b = []
+            for f in range(F):
+                one_feat = one_batch[f]
+                local_f = []
+                for t in range(T):
+                    curr_feat = one_feat[t]
+                    # if(curr_feat[2]==1):
+                    # TBM parameters
+                    beta_val = 0.75
+                    tau_val = 2
+                    h_val = 0.4
+                    m_t = curr_feat[2]
+                    x_l = curr_feat[0]
+                    x_m = curr_feat[1]
+                    curr_delta_t = curr_feat[3]
+                    b_t_dash = np.exp(-beta_val * curr_delta_t * 1.0 / tau_val)
+                    if b_t_dash > h_val:
+                        b_t = 1
+                    else:
+                        b_t = 0
+                    feat_val = (1 - m_t) * x_l + m_t * (b_t * x_l + (1 - b_t) * x_m)
+                    local_f.append(feat_val)
+                local_b.append(local_f)
+            raw_tbm.append(local_b)
 
-        raw_tbm = list((np.array(raw_tbm).transpose((1, 0, 2))).tolist())  # (num_features, time_seq, 1)
-        attn_clustered_tbm = []
+        # cluster attention
+        stack_data = []  # a list of (B, T), list len = num of clusters
         for cid, c in enumerate(self.cluster):
-            local = []
+            local_cluster = []
             for f in c:
-                local.append(torch.cuda.FloatTensor(raw_tbm[f]))
-            local = torch.stack(local, dim=1)  # (time_seq, cluster_len, 1)
-            attn = self.inner_attns[cid]((local, torch.cuda.LongTensor([len(c)])))  # (time_seq, 1)
-            attn_clustered_tbm.append(attn)
-        attn_clustered_tbm = torch.cat(tuple(attn_clustered_tbm), 1)  # (time_seq, cluster_num)
+                local_cluster.append(torch.cuda.FloatTensor(raw_tbm[:, ]))
 
-        attn_clustered_tbm.unsqueeze_(0)  # (1, time_seq, cluster_num)
-        attn_clustered_tbm = autograd.Variable(attn_clustered_tbm)
 
-        return attn_clustered_tbm
+
+        # time_seq = len(data)
+        # raw_tbm = []  # (time_seq, num_features, 1)
+        # for t in range(time_seq):
+        #     one_time = np.array(data[t])  # (37, 4)
+        #
+        #     local_tbm = []  # (37, 1)
+        #
+        #     for f in range(self.num_features):
+        #         curr_feat = one_time[f]
+        #         # if(curr_feat[2]==1):
+        #         # TBM parameters
+        #         beta_val = 0.75
+        #         tau_val = 2
+        #         h_val = 0.4
+        #         m_t = curr_feat[2]
+        #         x_l = curr_feat[0]
+        #         x_m = curr_feat[1]
+        #         curr_delta_t = curr_feat[3]
+        #         b_t_dash = np.exp(-beta_val * curr_delta_t * 1.0 / tau_val)
+        #         if b_t_dash > h_val:
+        #             b_t = 1
+        #         else:
+        #             b_t = 0
+        #         feat_val = (1 - m_t) * x_l + m_t * (b_t * x_l + (1 - b_t) * x_m)
+        #         local_tbm.append([feat_val])
+        #     raw_tbm.append(local_tbm)
+        #
+        # raw_tbm = list((np.array(raw_tbm).transpose((1, 0, 2))).tolist())  # (num_features, time_seq, 1)
+        # attn_clustered_tbm = []
+        # for cid, c in enumerate(self.cluster):
+        #     local = []
+        #     for f in c:
+        #         local.append(torch.cuda.FloatTensor(raw_tbm[f]))
+        #     local = torch.stack(local, dim=1)  # (time_seq, cluster_len, 1)
+        #     attn = self.inner_attns[cid]((local, torch.cuda.LongTensor([len(c)])))  # (time_seq, 1)
+        #     attn_clustered_tbm.append(attn)
+        # attn_clustered_tbm = torch.cat(tuple(attn_clustered_tbm), 1)  # (time_seq, cluster_num)
+        #
+        # attn_clustered_tbm.unsqueeze_(0)  # (1, time_seq, cluster_num)
+        # attn_clustered_tbm = autograd.Variable(attn_clustered_tbm)
+        #
+        # return attn_clustered_tbm
 
 
         # cvlised = []  # (time_seq, cluster_num)
@@ -290,22 +353,28 @@ class DotAttentionLayer(nn.Module):
         """
         input: (unpacked_padded_output: batch_size x seq_len x hidden_size, lengths: time_seq)
         """
-        inputs, lengths = input
+        inputs, lengths = input  # (B, T, H)
         batch_size, max_len, _ = inputs.size()
-        flat_input = inputs.contiguous().view(-1, self.hidden_size)
-        logits = self.W(flat_input).view(batch_size, max_len)
-        alphas = F.softmax(logits, dim=1)
+
+        flat_input = inputs.contiguous().view(-1, self.hidden_size)  # (B * T, H)
+        logits = self.W(flat_input).view(batch_size, max_len)  # (B * T, 1) ->  (B, T)
+
+        print('dot attn logits:', logits)
 
         # computing mask
         if is_cuda:
-            idxes = torch.arange(0, max_len, out=torch.LongTensor(max_len)).unsqueeze(0).cuda()
+            idxes = torch.arange(0, max_len, out=torch.LongTensor(max_len)).unsqueeze(0).cuda()  # (1, T)
         else:
             idxes = torch.arange(0, max_len, out=torch.LongTensor(max_len)).unsqueeze(0)
 
-        mask = autograd.Variable((idxes<lengths.unsqueeze(1)).float())
+        mask = autograd.Variable(torch.cuda.ByteTensor(idxes<lengths.unsqueeze(1)))  # (1, T)
+        mask = torch.cat([mask] * batch_size, dim=0)
+        print('dot attn mask: ', mask)
+        logits[~mask] = float('-inf')
+        alphas = F.softmax(logits, dim=1)  # (B, T)
 
-        alphas = alphas * mask
-        # renormalize
-        alphas = alphas / torch.sum(alphas, 1).view(-1, 1)
-        output = torch.bmm(alphas.unsqueeze(1), inputs).squeeze(1)
+        print('dot attn alpha: ', alphas)
+
+
+        output = torch.bmm(alphas.unsqueeze(1), inputs).squeeze(1)  # (B, 1, T) dot (B, T, H) -> (B, 1, H) -> (B, H)
         return output  # (batch_size, hidden_size)
