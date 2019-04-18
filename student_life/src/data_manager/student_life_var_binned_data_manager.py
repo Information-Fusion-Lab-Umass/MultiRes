07d9dad4eee0aaf75851b7c313dc92a104f8998b
Abhinav Shaw <abhinav.shaw1993@gmail.com>
2019-04-18 17:23:46 -0400

    Rafactoring of the previous two fixes!

--- student_life/src/data_manager/student_life_var_binned_data_manager.py
+++ student_life/src/data_manager/student_life_var_binned_data_manager.py
@@ -114,8 +114,8 @@ def process_student_data(raw_data, student_id: int,
     if ADJUST_LABELS_WRT_MEDIAN:
         y_labels['stress_level_mode'] = y_labels['stress_level_mode'].map(conversions.adjust_classes_wrt_median,
                                                                           na_action='ignore')
-        if 'previous_stress_label' in FEATURE_LIST:
-            training_values['previous_stress_label'] = training_values['previous_stress_label'].map(
+        if 'previous_stress_label' in COVARIATE_LIST:
+            covariate_values['previous_stress_label'] = covariate_values['previous_stress_label'].map(
                 conversions.adjust_classes_wrt_median,
                 na_action='ignore')
 
