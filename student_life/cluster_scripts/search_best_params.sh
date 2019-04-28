3dffc4d216d7936d0569370994800ca392e24936
Abhinav Shaw <abhinav.shaw1993@gmail.com>
2019-04-28 00:40:26 -0400

    Slurm script to run grid search

---
+++ student_life/cluster_scripts/search_best_params.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#
+#SBATCH --mem=15000
+#SBATCH --job-name=student-life-grid-search
+#SBATCH --partition=1080ti-long
+#SBATCH --output=grid_search-%A.out
+#SBATCH --error=grid_search-%A.err
+#SBATCH --mail-type=ALL
+#SBATCH --ntasks-per-node=6
+#SBATCH --nodes=1
+#SBATCH --mail-user=abhinavshaw@umass.edu
+
+# Log the jobid.
+echo $SLURM_JOBID - `hostname` >> ~/gypsum-jobs.txt
+
+cd ~/projects/MultiRes/student_life
+PYTHONPATH=../ python -m src.grid_search.grid_search
