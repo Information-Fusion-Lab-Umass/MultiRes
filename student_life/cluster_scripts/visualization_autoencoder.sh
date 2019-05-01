721204ae02812ed8d4d0faa137a9dab20457aed3
Abhinav Shaw <abhinav.shaw1993@gmail.com>
2019-05-01 19:28:51 -0400

    LSTM over different number of students experiments.

---
+++ student_life/cluster_scripts/visualization_autoencoder.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#
+#SBATCH --mem=10000
+#SBATCH --job-name=viz-mul
+#SBATCH --partition=1080ti-long
+#SBATCH --gres=gpu:1
+#SBATCH --output=visualization_autoencoder-%A.out
+#SBATCH --error=visualization_autoencoder-%A.err
+#SBATCH --mail-type=ALL
+#SBATCH --ntasks-per-node=6
+#SBATCH --nodes=1
+#SBATCH --time=2-00:00:00
+#SBATCH --mail-user=abhinavshaw@umass.edu
+
+# Log the jobid.
+echo $SLURM_JOBID - `hostname` >> ~/gypsum-jobs.txt
+
+cd ~/projects/MultiRes/student_life
+PYTHONPATH=../ python -m src.experiments.visualization.lstv_ae_over_different_no_students
