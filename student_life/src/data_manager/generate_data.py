61b30b9dc996391d3d0ac157ce8921428ec5768e
nsimsiri <nsimsiri@umass.edu>
2019-05-13 20:12:34 -0400

    running all studnent

--- student_life/src/data_manager/generate_data.py
+++ student_life/src/data_manager/generate_data.py
@@ -2,7 +2,8 @@ from src.data_manager import student_life_var_binned_data_manager as data_manage
 from src.bin import statistics
 from src.utils import write_utils
 
-student_list = [4, 7, 8, 10, 14, 16, 17, 19, 22, 23, 24, 32, 33, 35, 36, 43, 44, 49, 51, 52, 53, 57, 58]
+# student_list = [4, 7, 8, 10, 14, 16, 17, 19, 22, 23, 24, 32, 33, 35, 36, 43, 44, 49, 51, 52, 53, 57, 58]
+student_list = list(range(60))
 data = data_manager.get_data_for_training_in_dict_format(*student_list, normalize=True, fill_na=True,
                                                          flatten_sequence=False, split_type='percentage')
 print(statistics.get_train_test_val_label_counts_from_raw_data(data))
